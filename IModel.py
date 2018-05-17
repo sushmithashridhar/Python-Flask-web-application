cd3fc3c16822996e910de65670789a5e608a5c48
SUSHMITHA SHRIDHAR <32848089+sushmithashridhar@users.noreply.github.com>
2018-05-17 01:49:41 -0700

    Update IModel.py

--- IModel.py
+++ IModel.py
@@ -1,7 +1,18 @@
 from abc import ABC, abstractmethod
 
 class IModel(ABC):
+    """
+    This is a class for Model.
 
+    Attributes:
+    data (text): The data inserted to the sqlite3 database.
+    """
     @abstractmethod
-    def fetchall(self, dict):
+    def fetchall(self, data):
         pass
+    """
+    This is a fetchall function to fetch all the data inserted to sqlite3 database.
+
+    :param data: The data inserted to the sqlite3 database.
+    :returns: returns the data
+    """
