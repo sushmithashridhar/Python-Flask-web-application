97ccfe666f94a038e3b2f3d7118a347bc84a7af5
SUSHMITHA SHRIDHAR <32848089+sushmithashridhar@users.noreply.github.com>
2018-05-18 01:13:44 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,20 @@
-from flask import Flask, render_template, flash
+"""
+A simple reviews flask app.
+Data is stored in a SQLite database that looks something like the following:
+
++--------------+-------------+----------------------+------------+---------+-------+
+| termoffered | yearoffered | yearoftheweekoffered | instructor | review | rating |
++-------------+-------------+----------------------+------------+---------+-------+
+| Fall       |      2017   | Monday               | XYZ        |  Good  | 10     |
++------------+------------------+------------+----------------+
+
+This can be created with the following SQL (see bottom of this file):
+
+    create table reviews (termoffered text, yearoffered text, timeoftheweekoffered text, instructor text, review text, rating text;
+
+"""
+
+from flask import Flask, render_template, flash, request
 from main import Main
 from review import Review
 from model import AppModel
@@ -13,30 +29,57 @@ presenter = Presenter(model)
 def create_template(route):
     route_name = route.get_name()
 
-    if route.is_redirect():
-        return redirect(url_for(route_name))
-    else:
-        route_arg = route.get_args()
-        if (route_arg == None):
-            return render_template(route_name)
-        return render_template(route_name, **(route_arg))
+    route_arg = route.get_args()
+    if (route_arg == None):
+        return render_template(route_name)
+    return render_template(route_name, **(route_arg))
+"""
+This is a create_template function.
 
-def present_flash(f):
-    if (not (f == None)):
-        flash(f.get_msg(), f.get_msg_type().value)
+:param route: Specifies the route the html url containes.
+:returns: returns the route.
+"""
+
 
 def render_view(view):
-    present_flash(view.get_flash())
     return create_template(view.get_route())
+"""
+This is a render_view function.
 
-# Index
+:param view: View for route.
+:returns: returns the view for the route.
+"""
+
 @app.route('/')
 def index():
     return render_template(presenter.index())
+"""
+This is a index function.
+
+:param: No parameter passed.
+:returns: returns the url from index function from presenter.
+"""
 
 @app.route('/review')
 def review():
     return render_view(presenter.review())
+"""
+This is a review function.
+
+:param: No parameter passed.
+:returns: returns the url from review function from presenter.
+"""
+
+@app.route('/insert', methods = ['GET', 'POST'])
+def insert():
+    requestmethod = request.method == 'POST'
+    return render_template(presenter.insert(requestmethod, request.form))
+"""
+This is a insert function.
+
+:param: No parameter passed.
+:returns: returns the url after inserting data to the database.
+"""
 
 if __name__ == '__main__':
-	app.run(host = '0.0.0.0', port=8000)
+    app.run(host = '0.0.0.0', port=8000)
